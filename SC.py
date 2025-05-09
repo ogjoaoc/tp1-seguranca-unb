ebcb2d84aa6437b3b479c7cb1907eb1616e5c468
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-05-09 19:14:56 -0300

    add shift, genkeys and sboxes modules

--- SC.py
+++ SC.py
@@ -38,14 +38,48 @@ def ip_inverse(bits):
     
     return result
 
-def circular_shift():
-    pass
+def circular_shift(bits):
+    left = bits[:5]
+    right = bits[5:]
 
-def generate_keys():
-    pass
+    left = left[1:] + left[0]
+    right = right[1:] + right[0]
 
-def s_boxes():
-    pass
+    return left + right
+
+def generate_keys(chave):
+    K = perm10(chave)
+    K = circular_shift(K)
+    K1 = perm8(K)
+    K = circular_shift(circular_shift(K))
+    K2 = perm8(K)
+    return K1, K2
+
+def s_boxes(l4, r4):
+    # S-Box S0
+    S0 = [[1, 0, 3, 2],  # linha 0
+          [3, 2, 1, 0],  # linha 1
+          [0, 2, 1, 3],  # linha 2
+          [3, 1, 3, 2]]   # linha 3
+
+    # S-Box S1
+    S1 = [[0, 1, 2, 3],  # linha 0
+          [2, 0, 1, 3],  # linha 1
+          [3, 0, 1, 0],  # linha 2
+          [2, 1, 0, 3]]   # linha 3
+    
+    row_l = int(l4[0] + l4[3], 2)
+    col_l = int(l4[1] + l4[2], 2)
+    val_l = S0[row_l][col_l]
+
+    row_r = int(r4[0] + r4[3], 2)
+    col_r = int(r4[1] + r4[2], 2)
+    val_r = S1[row_r][col_r]
+
+    s0_out = format(val_l, '02b')
+    s1_out = format(val_r, '02b')
+
+    return s0_out + s1_out
 
 def rodada_feistel():
     pass
