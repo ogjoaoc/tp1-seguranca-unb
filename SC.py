b8a05fadc8d9c22d825bdfc677a89e8ee80a38ac
Aninha1105 <analuparaguassu01@gmail.com>
2025-05-09 11:36:18 -0300

    refactor perm10, perm8, perm4, ip, ip_inverse

--- SC.py
+++ SC.py
@@ -1,18 +1,42 @@
+def perm10(bits):
+    P10 = [3,  5,  2,  7,  4,  10,  1,  9,  8,  6]
+    result = ""
+    for i in P10:
+        result += bits[i-1]
+    
+    return result
 
-def perm10():
-    pass
+def perm8(bits):
+    P8 = [6,  3,  7,  4,  8,  5,  10,  9]
+    result = ""
+    for i in P8:
+        result += bits[i-1]
 
-def perm8():
-    pass
+    return result
 
-def perm4():
-    pass
+def perm4(bits):
+    P4 = [2,  4,  3,  1]
+    result = ""
+    for i in P4:
+        result += bits[i-1]
+    
+    return result
 
-def ip():
-    pass
+def ip(bits):
+    IP = [2,  6,  3,  1,  4,  8,  5,  7]
+    result = ""
+    for i in IP:
+        result += bits[i-1]
+    
+    return result
 
-def ip_inverse():
-    pass
+def ip_inverse(bits):
+    IP_R = [4,  1,  3,  5,  7,  2,  8,  6]
+    result = ""
+    for i in IP_R:
+        result += bits[i-1]
+    
+    return result
 
 def circular_shift():
     pass
@@ -38,3 +62,5 @@ def ecb_encrypt():
 def cbc_encrypt():
     pass
 
+bloco_de_dados = "11010111"
+chave = "1010000010"
