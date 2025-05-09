8af9ff6f0e063eb0f9420e169cf9e115346e1c5c
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-05-09 11:13:56 -0300

    init main modules

---
+++ SC.py
@@ -0,0 +1,40 @@
+
+def perm10():
+    pass
+
+def perm8():
+    pass
+
+def perm4():
+    pass
+
+def ip():
+    pass
+
+def ip_inverse():
+    pass
+
+def circular_shift():
+    pass
+
+def generate_keys():
+    pass
+
+def s_boxes():
+    pass
+
+def rodada_feistel():
+    pass
+
+def sdes_encrypt():
+    pass
+
+def sdes_decrypt():
+    pass
+
+def ecb_encrypt():
+    pass
+
+def cbc_encrypt():
+    pass
+
